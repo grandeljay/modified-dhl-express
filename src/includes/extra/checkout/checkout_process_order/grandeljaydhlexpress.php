d5f5f14246153c3480dd29f2f8b103ee13ba8f13
grandeljay <github@grandels.email>
2024-03-21 09:26:49 +0100

    fix: show proper shipping name without clutter

--- src/includes/extra/checkout/checkout_process_order/grandeljaydhlexpress.php
+++ src/includes/extra/checkout/checkout_process_order/grandeljaydhlexpress.php
@@ -10,11 +10,42 @@
 
 namespace Grandeljay\DhlExpress;
 
+/**
+ * Simplify and update shipping name
+ */
+$language_file = \sprintf(
+    DIR_FS_CATALOG . 'lang/%s/modules/shipping/%s.php',
+    $_SESSION['language'],
+    \grandeljaydhlexpress::class
+);
+
+require $language_file;
+
+$simplified_name = sprintf(
+    constant(Constants::MODULE_SHIPPING_NAME . '_TEXT_TITLE_WEIGHT'),
+    round($_SESSION['cart']->weight, 2)
+);
+
+/**
+ * Table `orders`
+ */
 $sql_data_array = [
-    'shipping_method' => sprintf(
-        constant(Constants::MODULE_SHIPPING_NAME . '_TEXT_TITLE_WEIGHT'),
-        round($_SESSION['cart']->weight, 2)
-    ),
+    'shipping_method' => $simplified_name,
 ];
 
-xtc_db_perform(TABLE_ORDERS, $sql_data_array, 'update', sprintf('orders_id = %d', $insert_id));
+\xtc_db_perform(\TABLE_ORDERS, $sql_data_array, 'update', sprintf('orders_id = %d', $insert_id));
+
+/**
+ * Table `orders_total`
+ */
+\xtc_db_query(
+    \sprintf(
+        'UPDATE `%s`
+            SET `title`     = "%s"
+          WHERE `orders_id` = %d
+            AND `class`     = "ot_shipping"',
+        \TABLE_ORDERS_TOTAL,
+        $simplified_name,
+        $insert_id
+    )
+);
