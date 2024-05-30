58e0902c8cca31fa47d8b6c053590e732f4dd646
grandeljay <github@grandels.email>
2024-05-30 12:15:49 +0200

    chore(module): update to v0.9.0

--- src/includes/modules/shipping/grandeljaydhlexpress.php
+++ src/includes/modules/shipping/grandeljaydhlexpress.php
@@ -19,7 +19,7 @@ class grandeljaydhlexpress extends StdModule
 {
     private Installer $installer;
 
-    public const VERSION     = '0.8.0';
+    public const VERSION     = '0.9.0';
     public array $properties = [];
 
     public static function weight(): string
