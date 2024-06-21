d938a1c45dd1072021f1368804b10af21fdb021c
grandeljay <github@grandels.email>
2024-06-21 11:40:26 +0200

    refactor: uninstaller into trait

---
+++ src-mmlc/Trait/Module/Uninstaller.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Grandeljay\DhlExpress\Trait\Module;
+
+use Grandeljay\DhlExpress\Zone;
+
+trait Installer
+{
+    private function removeConfigurations(): void
+    {
+        $this->removeConfiguration('ALLOWED');
+        $this->removeConfiguration('SORT_ORDER');
+
+        $this->removeConfigurationWeight();
+        $this->removeConfigurationShipping();
+        $this->removeConfigurationSurcharges();
+
+        $this->deleteAdminAccess(\grandeljaydhlexpress::class);
+    }
+
+    private function removeConfigurationWeight(): void
+    {
+        $this->removeConfiguration('WEIGHT');
+        $this->removeConfiguration('WEIGHT_IDEAL');
+        $this->removeConfiguration('WEIGHT_MAXIMUM');
+    }
+
+    private function removeConfigurationShipping(): void
+    {
+        $this->removeConfiguration('SHIPPING');
+
+        foreach (Zone::cases() as $zone) {
+            $number = $zone->value;
+
+            $configuration_key = sprintf('SHIPPING_ZONE%s', $number);
+
+            $this->removeConfiguration($configuration_key);
+        }
+
+        $this->removeConfiguration('SHIPPING_ZONE_PER_KG');
+    }
+
+    private function removeConfigurationSurcharges(): void
+    {
+        $this->removeConfiguration('SURCHARGES');
+        $this->removeConfiguration('PICK_PACK');
+    }
+}
--- src/includes/modules/shipping/grandeljaydhlexpress.php
+++ src/includes/modules/shipping/grandeljaydhlexpress.php
@@ -11,7 +11,7 @@
  * @phpcs:disable Squiz.Classes.ValidClassName.NotCamelCaps
  */
 
-use Grandeljay\DhlExpress\{Constants, Quote, Zone};
+ use Grandeljay\DhlExpress\{Constants, Quote};
 use Grandeljay\DhlExpress\Trait\Field;
 use Grandeljay\DhlExpress\Trait\Module;
 use RobinTheHood\ModifiedStdModule\Classes\StdModule;
@@ -96,42 +96,7 @@ class grandeljaydhlexpress extends StdModule
     {
         parent::remove();
 
-        $this->removeConfiguration('ALLOWED');
-        $this->removeConfiguration('SORT_ORDER');
-
-        $this->removeConfigurationWeight();
-        $this->removeConfigurationShipping();
-        $this->removeConfigurationSurcharges();
-
-        $this->installer->uninstallAdminAccess();
-    }
-
-    private function removeConfigurationWeight(): void
-    {
-        $this->removeConfiguration('WEIGHT');
-        $this->removeConfiguration('WEIGHT_IDEAL');
-        $this->removeConfiguration('WEIGHT_MAXIMUM');
-    }
-
-    private function removeConfigurationShipping(): void
-    {
-        $this->removeConfiguration('SHIPPING');
-
-        foreach (Zone::cases() as $zone) {
-            $number = $zone->value;
-
-            $configuration_key = sprintf('SHIPPING_ZONE%s', $number);
-
-            $this->removeConfiguration($configuration_key);
-        }
-
-        $this->removeConfiguration('SHIPPING_ZONE_PER_KG');
-    }
-
-    private function removeConfigurationSurcharges(): void
-    {
-        $this->removeConfiguration('SURCHARGES');
-        $this->removeConfiguration('PICK_PACK');
+        $this->removeConfigurations();
     }
 
     /**
