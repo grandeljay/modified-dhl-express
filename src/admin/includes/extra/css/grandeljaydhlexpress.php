1222ce759a1c3681deabc55cb8905bb29a6a216c
grandeljay <github@grandels.email>
2024-07-12 10:05:40 +0200

    feat: only load stylesheet on module page

--- src/admin/includes/extra/css/grandeljaydhlexpress.php
+++ src/admin/includes/extra/css/grandeljaydhlexpress.php
@@ -6,6 +6,14 @@ if (\rth_is_module_disabled(Constants::MODULE_SHIPPING_NAME)) {
     return;
 }
 
+if (
+       \FILENAME_MODULES !== \basename($PHP_SELF)
+    || !isset($_GET['module'])
+    || \grandeljaydhlexpress::class !== $_GET['module']
+) {
+    return;
+}
+
 $filename = 'includes/css/grandeljay_dhl_express.css';
 $version  = hash_file('crc32c', DIR_FS_ADMIN . $filename);
 ?>
